ad31b8149b631b33ad58a02fe38209e8ca28b905
Sachin Malesha <sachin.malesha@gmail.com>
2019-02-04 21:06:59 +0530

    Initial commit

---
+++ src/test/java/com/HelloWorldTest.java
@@ -0,0 +1,18 @@
+package com;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void printName() {
+    }
+}
